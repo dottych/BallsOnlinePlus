5b0b6c3f14241fb4a0ca33f18de3a536d75e4339
dottych <96998759+dottych@users.noreply.github.com>
2023-09-09 09:04:00 +0200

    Say current cosmetic

--- src/Messages/Commands/cosmetic.js
+++ src/Messages/Commands/cosmetic.js
@@ -4,7 +4,7 @@ const cosmetics = require('../../Lists').cosmetics;
 const tick = require('../../Tick');
 
 const c_cosmetic = (c, input) => {
-    if (input.trim !== "") {
+    if (input.trim() !== "") {
         if (input.trim().toLowerCase() === players.get(c.id).cosmetic) utils.msgClient(c, `You already have this cosmetic!`);
         else if (cosmetics.indexOf(input.trim()) >= 0) {
             players.get(c.id).cosmetic = input.trim().toLowerCase();
@@ -20,7 +20,7 @@ const c_cosmetic = (c, input) => {
 
             utils.msgClient(c, `You have changed your cosmetic to ${players.get(c.id).cosmetic}.`);
         } else utils.msgClient(c, `This cosmetic is invalid!`);
-    }
+    } else utils.msgClient(c, `Your current cosmetic is ${players.get(c.id).cosmetic}.`);
 }
 
 module.exports = c_cosmetic;
